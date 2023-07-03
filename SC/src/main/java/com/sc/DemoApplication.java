1d48aa46974efe0ae4e23a628fcc0ec2213b11e0
Phoenix0356 <1242780667@qq.com>
2023-07-03 22:04:45 +0800

    v1.0

--- SC/src/main/java/com/sc/DemoApplication.java
+++ SC/src/main/java/com/sc/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScApplication {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ScApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 
 }
---
+++ SC/src/main/java/com/sc/service/UserService.java
@@ -0,0 +1,7 @@
+package com.sc.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.sc.entity.User;
+
+public interface UserService extends IService<User> {
+}
