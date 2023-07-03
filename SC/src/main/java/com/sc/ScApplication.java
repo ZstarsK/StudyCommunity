c7199917902ccd67bfee166a94b88c3a903ef808
kzx <1262937829@qq.com>
2023-07-03 21:13:14 +0800

    initial commit

---
+++ SC/src/main/java/com/sc/ScApplication.java
@@ -0,0 +1,13 @@
+package com.sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScApplication.class, args);
+    }
+
+}
