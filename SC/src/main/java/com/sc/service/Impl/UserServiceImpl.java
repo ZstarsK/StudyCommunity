9bb9c2e96539075371b9ab42d35fc69785ee78b9
kzx <1262937829@qq.com>
2023-07-03 22:41:09 +0800

    11

--- SC/src/main/java/com/sc/service/Impl/UserServiceImpl.java
+++ SC/src/main/java/com/sc/service/Impl/UserServiceImpl.java
@@ -1,9 +1,9 @@
 package com.sc.service.Impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.ckgl.entity.User;
-import com.ckgl.mapper.UserMapper;
-import com.ckgl.service.UserService;
+import com.sc.entity.User;
+import com.sc.mapper.UserMapper;
+import com.sc.service.UserService;
 import org.springframework.stereotype.Service;
 //test
 @Service
